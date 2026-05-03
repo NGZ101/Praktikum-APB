d8b907647b0703f110f5ced80b7550e6bf65f42c
NGZ101 <162420225+NGZ101@users.noreply.github.com>
2026-05-03 22:58:06 +0700

    Delete Modul_6 directory

--- Modul_6/Unguided/function.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btn").addEventListener("click", function(){
-    let clicksound =new Audio("Kaching.mp3");
-    clicksound.play();
-    let thr = Math.floor(Math.random()*10000) + 10000;
-    document.getElementById("hasil").textContent = "Selamat Anda Mendapat THR Rp." + thr;
-});
-
